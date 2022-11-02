e57b49817dba446b31c9092b891d2be263825a5f
Oliver Evans <oliverevans96@gmail.com>
2022-11-02 02:56:37 -0700

    Add htop and tmux

--- nixos/default.nix
+++ nixos/default.nix
@@ -3,6 +3,8 @@
 {
   networking.hostName = "gandi-nixos-r1";
   system.stateVersion = "22.05";
+
   services.openssh.enable = true;
   programs.mosh.enable = true;
+  environment.systemPackages = with pkgs; [ htop tmux ];
 }
