0f2b767370144e64ac79a8890ab072c076d04ef7
Oliver Evans <oliverevans96@gmail.com>
2022-11-02 02:46:10 -0700

    Change hostname

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  networking.hostName = "test-hostname";
+  networking.hostName = "gandi-nixos-r1";
   system.stateVersion = "22.05";
   services.openssh.enable = true;
   programs.mosh.enable = true;
